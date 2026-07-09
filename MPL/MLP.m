61ae145ac9fc667143b3b8b049fd82f6a6349fae
zhangxin <12432359@mail.sustech.ecu.cn>
2026-07-09 14:00:48 +0800

    git commit -m "Added MLP and contact parameter estimation functions"

--- MPL/MLP.m
+++ MPL/MLP.m
@@ -452,34 +452,3 @@ xlabel('x / mm');
 ylabel('y / mm');
 axis equal;
 title('MLP prediction');
-
-%% ============================================================
-%  局部函数 1：
-%  根据 9 个传感器力值估计接触中心、半径、峰值力
-% ============================================================
-
-
-
-%% ============================================================
-%  局部函数 2：
-%  给定半径 r 时，计算高斯拟合误差
-% ============================================================
-
-function err = gaussian_fit_error(r, sensor_points, F_sensor, cx, cy, F_base)
-
-    F_sensor = F_sensor(:);
-
-    d2 = (sensor_points(:,1) - cx).^2 + ...
-         (sensor_points(:,2) - cy).^2;
-
-    g = exp(-d2 / (2 * r^2));
-
-    amp = (g' * (F_sensor - F_base)) / (g' * g);
-
-    amp = max(amp, 0);
-
-    F_fit = F_base + amp * g;
-
-    err = mean((F_fit - F_sensor).^2);
-
-end
---
+++ MPL/gaussian_fit_error.m
@@ -0,0 +1,23 @@
+%% ============================================================
+%  局部函数 2：
+%  给定半径 r 时，计算高斯拟合误差
+% ============================================================
+
+function err = gaussian_fit_error(r, sensor_points, F_sensor, cx, cy, F_base)
+
+    F_sensor = F_sensor(:);
+
+    d2 = (sensor_points(:,1) - cx).^2 + ...
+         (sensor_points(:,2) - cy).^2;
+
+    g = exp(-d2 / (2 * r^2));
+
+    amp = (g' * (F_sensor - F_base)) / (g' * g);
+
+    amp = max(amp, 0);
+
+    F_fit = F_base + amp * g;
+
+    err = mean((F_fit - F_sensor).^2);
+
+end
